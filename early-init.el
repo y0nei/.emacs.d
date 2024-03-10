7aea3052486491922e446c5c00683b7ff3309e25
y0nei <y0nei@proton.me>
2024-03-10 21:28:45 +0100

    feat: Early init file with performance optimization's

---
+++ early-init.el
@@ -0,0 +1,42 @@
+;;; early-init.el --- Early init file -*- lexical-binding: t; no-byte-compile: t -*-
+;;
+;; Copyright (c) 2024 Yonei
+;;
+;; Author: Yonei <y0nei@proton.me>
+;; Created: 11 March 2024
+;; Version: 0.0.1
+;; URL: https://codeberg.org/y0nei/.emacs.d
+;; Package-Requires: ((emacs "29.1"))
+;; SPDX-License-Identifier: MIT
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; Commentary:
+;; Since version 27, Emacs introduces an early init file. It is loaded before
+;; the package system and GUI is initialized. This file contains mainly startup
+;; optimizations.
+;;
+;;; Code:
+
+;; Prevent package.el loading packages prior to their init-file loading.
+(setq package-enable-at-startup nil)
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; Disable GUI elements (before they get a chance to be initialized)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Resize by pixels instead of cols/rows.
+(setq-default window-resize-pixelwise t
+              frame-resize-pixelwise t)
+
+;; Resizing the Emacs frame (to accommodate fonts that are smaller or larger
+;; than the system default font) appears to severely impact startup time. The
+;; larger the delta in font size, the greater the delay.
+(setq frame-inhibit-implied-resize t)
+
+;;; early-init.el ends here
